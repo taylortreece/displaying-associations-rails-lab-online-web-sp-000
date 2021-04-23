2379aa602920db1bde27e4600ec686e06e418b88
Taylor Treece <taylor.treece@protonmail.com>
2021-04-23 11:05:34 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
-Rails.application.routes.draw do
-  resources :artists, :songs
+ Rails.application.routes.draw do
+  resources :artists, only: [:index, :show]
+  resources :songs, only: [:index, :show]
 end
